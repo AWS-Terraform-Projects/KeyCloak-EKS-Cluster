77532fbf1e9c7fea70f0d8fc63e7348eee5b938f
Dennis Chacko <dennis.v.chacko@gmail.com>
2021-01-09 13:05:29 -0600

    extract vpc configuration to a module

--- main.tf
+++ main.tf
@@ -16,10 +16,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-## vpc setup
-
-data "aws_availability_zones" "available" {}
-
+#Setup the vpc
 locals {
   cluster_name = "training-eks-${random_string.suffix.result}"
 }
@@ -30,98 +27,19 @@ resource "random_string" "suffix" {
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.6.0"
-
-  name                 = "training-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
+  source = "./modules/vpc"
+  cluster_name = local.cluster_name
+  aws_region = var.aws_region
 }
 
-## vpc setup
-
-## security groups
-
-
-resource "aws_security_group" "worker_group_mgmt_one" {
-  name_prefix = "worker_group_mgmt_one"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
-}
-
-resource "aws_security_group" "worker_group_mgmt_two" {
-  name_prefix = "worker_group_mgmt_two"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
-  }
-}
-
-resource "aws_security_group" "all_worker_mgmt" {
-  name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
-    ]
-  }
-}
-
-## security groups
-
-## eks Cluster
-
+# Create the eks cluster
 module "eks" {
   source = "./modules/eks"
   create_cluster  = var.create_cluster 
-  aws_security_group_worker_group_mgmt_one = aws_security_group.worker_group_mgmt_one.id
-  aws_security_group_worker_group_mgmt_two = aws_security_group.worker_group_mgmt_two.id
+  aws_security_group_worker_group_mgmt_one = module.vpc.aws_security_group_worker_group_mgmt_one
+  aws_security_group_worker_group_mgmt_two = module.vpc.aws_security_group_worker_group_mgmt_two
   vpc_id = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
   cluster_name = local.cluster_name
   count = var.create_cluster == "yes" ? 1 : 0
 }
-
-## eks cluster
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "cluster_name" {
+  description = ""
+}
+
+variable "aws_region" {
+  description = "AWS region this api gateway needs to be deployed"
+}
